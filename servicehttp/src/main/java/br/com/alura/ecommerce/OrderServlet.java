80d6679aa0884d3863daa1edfe3cb25df75c3237
Tulio <tulio.parreira@helpper.com.br>
2021-02-02 09:36:37 -0300

    Criando servidor http como ponto de entrada.

---
+++ servicehttp/src/main/java/br/com/alura/ecommerce/OrderServlet.java
@@ -0,0 +1,57 @@
+package br.com.alura.ecommerce;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
+public class OrderServlet extends HttpServlet {
+
+
+    private final KafkaDispatcher<Order> orderDispatcher = new KafkaDispatcher<>();
+    private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        orderDispatcher.close();
+        emailDispatcher.close();
+    }
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            // we are not caring about any security issues
+            // we are hot to use http as start point
+            var email = req.getParameter("email");
+            var amount = new BigDecimal(req.getParameter("amount"));
+            var orderId = UUID.randomUUID().toString();
+
+            var order = new Order(orderId, amount, email);
+            orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, order);
+
+            var emailBody = "Welcome! Thank you for your order! We are processing your order!";
+            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, emailBody);
+
+            System.out.println("New order send successfully");
+            resp.setStatus(HttpServletResponse.SC_OK);
+            resp.getWriter().println("New order send successfully");
+        } catch (ExecutionException e) {
+            throw new ServletException(e);
+        } catch (InterruptedException e) {
+            throw new ServletException(e);
+        }
+
+
+    }
+}
